afdc5e37eff47d9c8f29a3f501ea7a93194320de
kselnaag <kselnaag@gmail.com>
2023-03-17 18:13:33 +0300

    add HealthChech to pre-commit

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-echo -e "\n>>_ShortLink build_<<\n"
 go build -o ./bin/shortlink ./cmd/main.go
-if [[ $? -gt 0 ]]; then exit 1; fi
-
 ./bin/shortlink
--- cicd/checks.sh
+++ cicd/checks.sh
@@ -16,3 +16,18 @@ if [[ $? -gt 0 ]]; then exit 1; fi
 echo -e "\n>>_ShortLink build_<<"
 go build -o ./bin/shortlink ./cmd/main.go
 if [[ $? -gt 0 ]]; then exit 1; fi
+
+echo -e "\n>>_ServerStart_<<"
+./bin/shortlink 1>/dev/null &
+SERVERPID=$!
+if [[ $? -gt 0 ]]; then exit 1; fi
+
+echo -e "\n>>_HealthCheck_<<"
+sleep 2
+RESPCODE=`curl -i http://localhost:8080/check/ping 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+if [ "$RESPCODE" != "200" ]; then 
+    exit 1;
+fi
+
+echo -e "\n>>_CloseServer_<<\n"
+kill $SERVERPID
