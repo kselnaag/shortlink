77a101fc3f26c7b573c685fce61802b7b05c8f37
kselnaag <kselnaag@gmail.com>
2023-03-17 15:51:00 +0300

    add github actions

--- cicd/checks.sh
+++ cicd/checks.sh
@@ -16,7 +16,3 @@ if [[ $? -gt 0 ]]; then exit 1; fi
 echo -e "\n>>_ShortLink build_<<"
 go build -o ./bin/shortlink ./cmd/main.go
 if [[ $? -gt 0 ]]; then exit 1; fi
-
-echo -e "\n>>_HelthChecks_<<"
-echo -e "empty\n"
-if [[ $? -gt 0 ]]; then exit 1; fi
