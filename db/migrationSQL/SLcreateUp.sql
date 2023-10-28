4faa2b671878abdfd19b5f17cf5bb7d7759449db
kselnaag <kselnaag@gmail.com>
2023-10-28 19:39:34 +0300

    del db dir (not used)

--- db/migrationSQL/SLcreateUp.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS shortlink (
-    slink TEXT PRIMARY KEY,
-    llink TEXT NOT NULL,
-    CHECK (llink <> '')
-);
--- db/migrationSQL/SLinitUp.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO shortlink VALUES ('5clp60', 'http://lib.ru');
-INSERT INTO shortlink VALUES ('dhiu79', 'http://google.ru');
